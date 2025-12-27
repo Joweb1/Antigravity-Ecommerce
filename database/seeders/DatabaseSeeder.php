21e33586a1cb0ebc2fa803d51ab40bc5b7fb1292
Jovibe Code <nahjonah00@gmail.com>
2025-12-27 09:12:53 +0100

    fix: storage issues

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(ProductSeeder::class); // Call the ProductSeeder
-
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(ProductSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
